22d258c13e8eb296f85d485d418b807b6e0cac6e
malsherer <mgracesherer@gmail.com>
2023-11-01 11:35:42 -0700

    devise configurations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
+user2 = User.where(email: "test2@example.com").first_or_create(password: "password", password_confirmation: "password")
+
+Post.create(
+    title: 'post1',
+    time_spent: '3 hours',
+    difficulty: 5,
+    price: '$100',
+    review: 'sample review',
+    image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
+    user_id: user1.id
+)
+
+Post.create(
+    title: 'post2',
+    time_spent: '4 hours',
+    difficulty: 8,
+    price: '$105',
+    review: 'sample review 2',
+    image: 'https://unsplash.com/photos/white-and-black-lego-toy-B7N0IjiIJYo',
+    user_id: user2.id
+)
+
+p Post.all
+p User.all
