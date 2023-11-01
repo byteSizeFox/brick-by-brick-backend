f4c91dd96a088a1b2274ea0e2c63dd0ae09b57ae
aesandino <anabellasandino@gmail.com>
2023-11-01 13:54:45 -0700

    rails index functionality

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe "Posts", type: :request do 
+  let(:user) { User.create(
+    email: 'user1@email.com', 
+    password: 'password',
+    password_confirmation: 'password'
+    )
+  }
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'gets a lists of posts' do
+      post = user.posts.create(
+        title: 'post1',
+        time_spent: '3 hours',
+        difficulty: 5,
+        price: '$100',
+        review: 'sample review',
+        image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
+        user_id: 1
+      )
+      get '/posts' 
+
+    post = JSON.parse(response.body)
+    expect(response).to have_http_status(200) 
+    expect(post.first['title']).to eq('post1')
+    end
   end
 end
