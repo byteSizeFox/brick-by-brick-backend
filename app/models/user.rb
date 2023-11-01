420e5c34f20f529c58a312a5440537718fce8989
malsherer <mgracesherer@gmail.com>
2023-11-01 13:28:20 -0700

    finished jwt and devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
   has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_01_181900) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_01_202108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "image"
