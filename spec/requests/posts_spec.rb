c328169a8e29e03036f5772f3d5153c56b740ec8
aesandino <anabellasandino@gmail.com>
2023-11-01 16:28:56 -0700

    completed the test for title

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -17,13 +17,50 @@ RSpec.describe "Posts", type: :request do
         price: '$100',
         review: 'sample review',
         image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
-        user_id: 1
+        user_id: user.id
       )
       get '/posts' 
 
     post = JSON.parse(response.body)
+    expect(post.first['title']).to eq 'post1'
     expect(response).to have_http_status(200) 
-    expect(post.first['title']).to eq('post1')
     end
   end
-end
+  describe "POST /create" do
+    it 'saves a valid entry into the database' do
+      post_params = {
+        post: {
+          title: 'post1',
+          time_spent: '3 hours',
+          difficulty: 5,
+          price: '$100',
+          review: 'sample review',
+          image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
+          user_id: user.id
+         }
+    }
+    post '/posts', params: post_params
+    post = JSON.parse(response.body)
+    expect(post['title']).to eq 'post1'
+    expect(response).to have_http_status(200)
+  end
+  it 'does not save an entry missing a title' do
+        post_params = {
+          post: {
+            title: '',
+            time_spent: '3 hours',
+            difficulty: 5,
+            price: '$100',
+            review: 'sample review',
+            image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
+            user_id: user.id
+           }
+      }
+      post '/posts', params: post_params
+      post_error = JSON.parse(response.body)
+      expect(post_error['title']).to include("can't be blank")
+      expect(response).to have_http_status(422)
+  end
+
+  end
+end 
