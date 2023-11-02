9333ceaa019b20c1962395437958df696d0424a6
aesandino <anabellasandino@gmail.com>
2023-11-02 15:24:04 -0700

    partially completed the validations, still working on it

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+
+  it 'should validate title' do
+    post = Post.create(
+      title: nil,
+      time_spent: '3 hours',
+      difficulty: 5,
+      price: '$100',
+      review: 'sample review',
+      image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
+      user_id: user.id
+    )
+      expect(post.errors[:title]).to_not be_empty
+
+  end
+end 
