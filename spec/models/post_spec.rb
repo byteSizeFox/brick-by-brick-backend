ea2a34962194690fedde14690ad8bd20dc82e0ef
aesandino <anabellasandino@gmail.com>
2023-11-05 12:45:03 -0800

    completed validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,9 +10,77 @@ RSpec.describe Post, type: :model do
       price: '$100',
       review: 'sample review',
       image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
-      user_id: user.id
+      user_id: 1
     )
       expect(post.errors[:title]).to_not be_empty
 
   end
+  it 'should validate time_spent' do
+    post = Post.create(
+      title: 'post1',
+      time_spent: nil,
+      difficulty: 5,
+      price: '$100',
+      review: 'sample review',
+      image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
+      user_id: 1
+    )
+      expect(post.errors[:time_spent]).to_not be_empty
+  
+end 
+
+it 'should validate difficulty' do
+  post = Post.create(
+    title: 'post1',
+    time_spent: '3 hours',
+    difficulty: nil,
+    price: '$100',
+    review: 'sample review',
+    image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
+    user_id: 1
+  )
+    expect(post.errors[:difficulty]).to_not be_empty
+
+end 
+
+it 'should validate price' do
+  post = Post.create(
+    title: 'post1',
+    time_spent: '3 hours',
+    difficulty: 5,
+    price: nil,
+    review: 'sample review',
+    image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
+    user_id: 1
+  )
+    expect(post.errors[:price]).to_not be_empty
+
+end 
+
+it 'should validate review' do
+  post = Post.create(
+    title: 'post1',
+    time_spent: '3 hours',
+    difficulty: 5,
+    price: '$100',
+    review: nil,
+    image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
+    user_id: 1
+  )
+    expect(post.errors[:review]).to_not be_empty
+
+end 
+
+it 'should validate image' do
+  post = Post.create(
+    title: 'post1',
+    time_spent: '3 hours',
+    difficulty: 5,
+    price: '$100',
+    review: 'sample review',
+    image: nil,
+    user_id: 1
+  )
+    expect(post.errors[:image]).to_not be_empty
+
 end 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ it 'is not valid without an email' do
+  user = User.create(
+    password: "password", 
+    password_confirmation: "password"
+  )
+  expect(user.errors[:email]).to_not be_empty
+ end 
+ it 'is not valid without a password' do
+    user = User.create(
+    email: "test1@example.com",
+    password_confirmation: "password"
+    )
+    expect(user.errors[:password]).to_not be_empty
+ end
+ it 'is not valid without a password confirmation' do
+    user = User.create(
+    email: "test1@example.com",
+    password: "password"
+    )
+    expect(user.errors[:password_confirmation]).to_not be_empty
+ end
+ context 'without an @ sign' do
+    let(:email) { 'testexample.com' }
+
+    it 'is not valid' do
+      expect(subject).to_not be_valid
+      expect(subject.errors[:email]).to include('is invalid')
+    end
+  end
 end
+
+
