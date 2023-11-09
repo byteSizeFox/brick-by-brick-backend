b7b56939df0d3f18621bc668592a5d02ae48c1a2
Shaun Stephenson <shaunfitzgarald@gmail.com>
2023-11-09 14:14:45 -0800

    changed db table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
-user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
-user2 = User.where(email: "test2@example.com").first_or_create(password: "password", password_confirmation: "password")
+user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password", username: "test1")
+user2 = User.where(email: "test2@example.com").first_or_create(password: "password", password_confirmation: "password", username: "test2")
 
 Post.create(
-    title: 'post1',
+    title: 'Bombtastic Build',
     time_spent: '3 hours',
     difficulty: 5,
     price: '$100',
     review: 'sample review',
     image: 'https://unsplash.com/photos/lego-mini-figure-on-brown-sand-kgz9vsP5JCU',
-    user_id: 1
+    user_id: user1.id,
+    username: 'shauneigh'
 )
 
 Post.create(
@@ -18,7 +19,8 @@ Post.create(
     price: '$105',
     review: 'sample review 2',
     image: 'https://unsplash.com/photos/white-and-black-lego-toy-B7N0IjiIJYo',
-    user_id: 2
+    user_id: user2.id,
+    username: 'TheBrizeulator'
 )
 
 p Post.all
